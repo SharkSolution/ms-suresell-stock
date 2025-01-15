0281d7d338082d39af75762962a05d811a81acb9
santiagoagudelo <agudelo.santigo@gmail.com>
2025-01-14 20:18:20 -0500

    Loombok solution

--- src/main/java/org/blackequity/shared/dto/ProductDTO.java
+++ src/main/java/org/blackequity/shared/dto/ProductDTO.java
@@ -1,12 +1,10 @@
 package org.blackequity.shared.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
 public class ProductDTO {
     private Long id;
     private String name;
